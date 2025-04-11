2c87ad5029186bb0a8004b721138c3f5da253b73
Charles Paxson <chpaxson@berkeley.edu>
2025-04-10 17:04:35 -0700

    Briefly implement multicore

--- Software/motor_controller/motorControllerFW/main.cpp
+++ Software/motor_controller/motorControllerFW/main.cpp
@@ -5,9 +5,10 @@
 #include <stdio.h>
 #include <string.h>
 #include "pico/stdlib.h"
+#include "stepper.h"
+#include "pico/multicore.h"
 #include "pico/binary_info.h"
 #include "hardware/adc.h"
-#include "stepper.h"
 
 #include "MT6701.h"
 
@@ -23,15 +24,43 @@ const uint8_t stepper_pin_2A = 8;
 const uint8_t stepper_pin_2B = 9;
 const uint16_t stepper_steps_per_revolution = 200;
 const stepper_mode_t stepping_mode = power;
-uint8_t speed = 20;
 
 
 /*******************************************************************************
 * Main
 */
 
+void core1_main() {
+
+    printf("Entered core0 (core=%d)\n", get_core_num());
+
+    // Initialise the stepper
+    stepper_init(&stepper, stepper_pin_1A, stepper_pin_1B,
+        stepper_pin_2A, stepper_pin_2B,
+        stepper_steps_per_revolution, stepping_mode);
+
+    while (1) {
+        stepper_set_speed_rpm(&stepper, 50);
+        stepper_rotate_degrees(&stepper, 270);
+        sleep_ms(500);
+
+        stepper_set_speed_rpm(&stepper, 150);
+        stepper_rotate_degrees(&stepper, 360);
+
+        stepper_release(&stepper);
+        printf("Finished a stepper loop");
+        sleep_ms(4000);
+    }
+
+}
+
 int main() {
     stdio_init_all();
+
+    printf("Entered core0 (core=%d)\n", get_core_num());
+
+    multicore_launch_core1(core1_main);
+
     i2c_init(I2C_PORT, 400 * 1000);  // Initialize I2C with 100kHz clock
     adc_init();
 
@@ -44,12 +73,6 @@ int main() {
     adc_gpio_init(27); // Current B input 1
     adc_gpio_init(28); // MT6701 input 2
     adc_gpio_init(29); // VBUS input 3
-
-    // Initialise the stepper
-    stepper_init(&stepper, stepper_pin_1A, stepper_pin_1B,
-            stepper_pin_2A, stepper_pin_2B,
-            stepper_steps_per_revolution, stepping_mode);
-    stepper_set_speed_rpm(&stepper, speed);
 
     sleep_ms(5000);
     printf("Start... \n");
@@ -67,30 +90,6 @@ int main() {
         angle = angle_read(data);
   
         printf("Angle: %.2f | Angle Analog Val: %.2f| V_BUS Analog Val: %u \r\n", angle, analog_angle_read(), VBUS);
-
-
-        // Rotate 3/4 of a turn.
-        stepper_rotate_degrees(&stepper, 270);
-        sleep_ms(500);
-
-        // Now rotate these many steps in the oposite direction
-        stepper_rotate_steps(&stepper, -45);
-        sleep_ms(500);
-
-        // Increase the speed and rotate 360 degrees
-        speed = 50;
-        stepper_set_speed_rpm(&stepper, speed);
-        stepper_rotate_degrees(&stepper, 360);
-
-        // Release the coils and sleep for a while. You can check that
-        // the coils are not energised by moving the rotor manually:
-        // there should be little resistance.
-        stepper_release(&stepper);
-        sleep_ms(4000);
-
-        // Decrease the speed
-        speed = 15;
-        stepper_set_speed_rpm(&stepper, speed);
 
         sleep_ms(10);  // Read every second
     }
