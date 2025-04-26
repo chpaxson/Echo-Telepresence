a0c3f0dbb4702f60e1908c000059ff2dc2d1bd9a
ishaangupta04 <ishaan.gupta@berkeley.edu>
2025-04-26 13:03:56 -0700

    CAN Setup up ESP32

--- Software/CAN-TESTING/CAN_Testing/CAN_Testing.ino
+++ Software/CAN-TESTING/CAN_Testing/CAN_Testing.ino
@@ -1,6 +1,7 @@
 //==================================================================================//
 
 #include <CAN.h>
+#include <Arduino.h>
 
 #define TX_GPIO_NUM   15  // Connects to CTX
 #define RX_GPIO_NUM   14  // Connects to CRX
@@ -8,97 +9,15 @@
 //==================================================================================//
 
 void setup() {
-  Serial.begin (115200);
-  while (!Serial);
-  delay (1000);
-
-  Serial.println ("CAN Receiver/Receiver");
-
-  // Set the pins
-  CAN.setPins (RX_GPIO_NUM, TX_GPIO_NUM);
-
-  // start the CAN bus at 500 kbps
-  if (!CAN.begin (500E3)) {
-    Serial.println ("Starting CAN failed!");
-    while (1);
-  }
-  else {
-    Serial.println ("CAN Initialized");
-  }
+ pinMode( TX_GPIO_NUM, INPUT );
+ pinMode( RX_GPIO_NUM, INPUT );
+ Serial.begin( 115200 ); // 1Mbaud, CAN Bus is most likely 500kHz
 }
-
-//==================================================================================//
-
 void loop() {
-  // canSender();
-  canReceiver();
+ int a1Value = analogRead( TX_GPIO_NUM );
+ int a2Value = analogRead( RX_GPIO_NUM );
+ Serial.print( "A1: " );
+ Serial.println( a1Value );
+ Serial.print( "A2: " );
+ Serial.println( a2Value );
 }
-
-//==================================================================================//
-
-void canSender() {
-  // send packet: id is 11 bits, packet can contain up to 8 bytes of data
-  Serial.print ("Sending packet ... ");
-
-  CAN.beginPacket (0x12);  //sets the ID and clears the transmit buffer
-  // CAN.beginExtendedPacket(0xabcdef);
-  CAN.write ('1'); //write data to buffer. data is not sent until endPacket() is called.
-  CAN.write ('2');
-  CAN.write ('3');
-  CAN.write ('4');
-  CAN.write ('5');
-  CAN.write ('6');
-  CAN.write ('7');
-  CAN.write ('8');
-  CAN.endPacket();
-
-  //RTR packet with a requested data length
-  CAN.beginPacket (0x12, 3, true);
-  CAN.endPacket();
-
-  Serial.println ("done");
-
-  delay (1000);
-}
-
-//==================================================================================//
-
-void canReceiver() {
-  // try to parse packet
-  int packetSize = CAN.parsePacket();
-
-  if (packetSize) {
-    // received a packet
-    Serial.print ("Received ");
-
-    if (CAN.packetExtended()) {
-      Serial.print ("extended ");
-    }
-
-    if (CAN.packetRtr()) {
-      // Remote transmission request, packet contains no data
-      Serial.print ("RTR ");
-    }
-
-    Serial.print ("packet with id 0x");
-    Serial.print (CAN.packetId(), HEX);
-
-    if (CAN.packetRtr()) {
-      Serial.print (" and requested length ");
-      Serial.println (CAN.packetDlc());
-    } else {
-      Serial.print (" and length ");
-      Serial.println (packetSize);
-
-      // only print packet data for non-RTR packets
-      while (CAN.available()) {
-        Serial.print ((char) CAN.read());
-      }
-      Serial.println();
-    }
-
-    Serial.println();
-  }
-}
-
-//==================================================================================//
